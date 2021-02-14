5d18fea4e271d4ff9f3c0ad08a4f66262697586a
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2021-02-14 18:29:11 +0100

    Add error handling

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -1,3 +1,4 @@
+// https://blog.questionable.services/article/http-handler-error-handling-revisited/
 package http
 
 import (
@@ -5,6 +6,29 @@ import (
 
 	"github.com/sophiabrandt/go-rest-api/internal/env"
 )
+
+// Error represents a handler error. It provides methods for a HTTP status
+// code and embeds the built-in error interface.
+type Error interface {
+	error
+	Status() int
+}
+
+// StatusError represents an error with an associated HTTP status code.
+type StatusError struct {
+	Code int
+	Err  error
+}
+
+// Allows StatusError to satisfy the error interface.
+func (se StatusError) Error() string {
+	return se.Err.Error()
+}
+
+// Returns our HTTP status code.
+func (se StatusError) Status() int {
+	return se.Code
+}
 
 // Handler takes a configured Env.
 type Handler struct {
@@ -16,6 +40,17 @@ type Handler struct {
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	err := h.H(h.E, w, r)
 	if err != nil {
-		h.E.Router.ServeHTTP(w, r)
+		switch e := err.(type) {
+		case Error:
+			// We can retrieve the status here and write out a specific
+			// HTTP status code.
+			h.E.Log.Printf("HTTP %d - %s", e.Status(), e)
+			http.Error(w, e.Error(), e.Status())
+		default:
+			// Any error types we don't specifically look out for default
+			// to serving a HTTP 500
+			http.Error(w, http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError)
+		}
 	}
 }
